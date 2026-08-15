d3d3508d2f1a10b4e5ee410bfd4189b361029b8d
mitpatel807 <mitpatel807@yahoo.com>
2026-08-15 02:55:43 +0100

    Day 9: Checkout Flow

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   reporter: 'html',
-  retries: process.env.CI ? 2 : 0 ,
+  retries: process.env.CI ? 2 : 0,
   use: {
     trace: 'on-first-retry',
     baseURL: "https://practicesoftwaretesting.com",
