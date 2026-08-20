9abeeebf5fc78baabaf65e36f333e0503829afef
Mit Patel <87959696+Mitpatel11@users.noreply.github.com>
2026-08-20 03:25:11 +0100

    Day 14: Page Manager and Refactor (#13)

--- tests/e2e/checkout.spec.js
+++ tests/e2e/checkout.spec.js
@@ -1,16 +1,12 @@
 const { test, expect } = require('@playwright/test');
-const { BasePage } = require('../../pages/BasePage');
-const { ProductPage } = require('../../pages/ProductPage');
-const { CartPage } = require('../../pages/CartPage');
-const { CheckoutPage } = require('../../pages/CheckoutPage');
+const { PageManager } = require('../../pages/PageManager');
 
 
 test.describe('Checkout Flow', () => {
-    let basePage;
-
+    let pages;
     test.beforeEach(async ({ page }) => {
-        basePage = new BasePage(page);
-        await basePage.goTo();
+        pages = new PageManager(page);
+        await pages.productPage.goTo('/');
     });
 
 
@@ -19,41 +15,33 @@ test.describe('Checkout Flow', () => {
     const password = process.env.USER_PASSWORD;
     test('comeplete the checkout and place order', async ({ page }) => {
         const item = 'Hammer';
-        const loginPage = await basePage.goToSignin();
-        await loginPage.login(email, password);
+        await pages.basePage.goToSignin();
+        await pages.loginPage.login(email, password);
 
         await expect(page).toHaveURL(/account/);
-        await basePage.goToHome();
+        await pages.basePage.goToHome();
 
-        const productPage = new ProductPage(page);
-        await productPage.searchItem(item);
-        const product = productPage.filterProduct(item);
+        await pages.productPage.searchItem(item);
+        const product = pages.productPage.filterProduct(item);
         await expect(product).toHaveCount(1);
         await product.click();
 
-        await expect(productPage.productName).toHaveText(item);
+        await expect(pages.productPage.productName).toHaveText(item);
 
-        await productPage.addToCart();
-        //await expect(page.locator('#toast-container')).toContainText('Product added to shopping cart.');
+        await pages.productPage.addToCart();
+        await expect(pages.productPage.toast).toContainText('Product added to shopping cart.');
+        await pages.productPage.goToCart();
 
-        await expect(productPage.toast).toContainText('Product added to shopping cart.');
-        //await page.getByRole('link', { name: 'cart' }).click();
-        await productPage.goToCart();
-
-        const cartPage = new CartPage(page);
-        const cartRow = cartPage.VerifyProduct(item);
+        const cartRow = pages.cartPage.VerifyProduct(item);
         await expect(cartRow).toHaveCount(1);
         await expect(cartRow).toContainText(item);
-        await cartPage.proceedToCheckout();
+        await pages.cartPage.proceedToCheckout();
 
-
-        const checkoutPage = new CheckoutPage(page);
         await expect(page.getByText("You can proceed to checkout.")).toBeVisible();
-        await checkoutPage.checkoutToAddress();
+        await pages.checkoutPage.checkoutToAddress();
 
-        //await page.locator("#country").selectOption("Ireland");
 
-        await expect(page.getByLabel("Street")).not.toHaveValue('');   // proves the one-shot auto-fill already ran
+        await expect(page.getByLabel("Street")).not.toHaveValue('');
 
         const address = {
             postalCode: 'D01X000',
@@ -62,27 +50,18 @@ test.describe('Checkout Flow', () => {
             city: 'Dublin',
             state: 'Leinster',
         };
-        await checkoutPage.fillAddress(address);
-
-        // await page.getByLabel("Postal Code").fill('D01X000');
-        // await page.getByLabel("House number").fill('130');
-        // await page.getByLabel("Street").press('Tab');
-        // await page.getByLabel("City").press('Tab');
-
-        //  await page.getByLabel("Postal Code").fill('D01X000');
-        //  await page.getByLabel("House number").fill('130');
-        //  await page.getByLabel("State").fill("Leinster");
+        await pages.checkoutPage.fillAddress(address);
 
 
-        await checkoutPage.checkoutToPayments();
+        await pages.checkoutPage.checkoutToPayments();
         const paymethod = "Cash on Delivery";
-        await checkoutPage.paymentMethodSelect(paymethod);
+        await pages.checkoutPage.paymentMethodSelect(paymethod);
 
         await expect(page.getByText("Payment was successful")).toBeVisible();
         await expect(page.getByRole('button', { name: 'Confirm' })).toBeVisible();
-        await checkoutPage.ConfirmOrder();
+        await pages.checkoutPage.ConfirmOrder();
 
-        const invoiceNum = await checkoutPage.getInvoiceNumber();
+        const invoiceNum = await pages.checkoutPage.getInvoiceNumber();
         console.log(invoiceNum);
 
     });
--- tests/e2e/search-product.spec.js
+++ tests/e2e/search-product.spec.js
@@ -1,13 +1,15 @@
 const { test, expect } = require('@playwright/test');
+const { PageManager } = require('../../pages/PageManager');
+
 test.describe('Search A Product', () => {
     test('Search product and assert', async ({ page }) => {
-        await page.goto("/");
-        await page.getByPlaceholder("Search").fill("Hammer");
-        await page.getByRole('button', { name: "Search" }).click();
-
-        // Catch all the elementes displayed after search
-        const results = await page.locator('[data-test="product-name"]');
-        await expect(results.first()).toBeVisible();
-        await expect(results.first()).toContainText("Hammer");
+        let pages;
+        pages = new PageManager(page);
+        await pages.productPage.goTo();
+        const item = 'Hammer';
+         await pages.productPage.searchItem(item);
+        const product = pages.productPage.filterProduct(item);
+        await expect(product).toHaveCount(1);
+        await product.click();
     });
 });
